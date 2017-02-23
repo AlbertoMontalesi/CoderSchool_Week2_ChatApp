2cd04227e5f7a4de28e969032a8bbcb133165027
AlbertoMontalesi <alberto.montalesi@outlook.it>
2017-02-23 20:11:50 +0700

    fix and layuout improvement

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,10 @@
 class MessagesController < ApplicationController
 
   before_action :require_user #if you use devise authenticate user will be already here
+  def index
+      @message = Message.all
+      
+  end
 
   def incoming
     @incoming_messages = current_user.incoming_messages
@@ -8,7 +12,16 @@ class MessagesController < ApplicationController
 
   def new
     @message = Message.new
+  end
 
+  def create  #still not working 
+     @message = Message.new
+  if @message.save
+    redirect_to incoming_messages_path
+  else
+    render new_message_path
+    flash[:success] = "Message was not sent "
+  end
   end
 
 
